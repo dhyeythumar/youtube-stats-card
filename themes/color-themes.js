723d977008fadf3e387c00076551a4d35916b6c9
Dhyey Thumar <dhyeythumar@gmail.com>
2020-12-09 21:42:59 +0530

    add color themes

---
+++ themes/color-themes.js
@@ -0,0 +1,88 @@
+const color_themes = {
+    default: {
+        title_color: "2f80ed",
+        icon_color: "4c71f2",
+        text_color: "333",
+        bg_color: "fffefe",
+    },
+    dark: {
+        title_color: "fff",
+        icon_color: "79ff97",
+        text_color: "9f9f9f",
+        bg_color: "151515",
+    },
+    radical: {
+        title_color: "fe428e",
+        icon_color: "f8d847",
+        text_color: "a9fef7",
+        bg_color: "141321",
+    },
+    merko: {
+        title_color: "abd200",
+        icon_color: "b7d364",
+        text_color: "68b587",
+        bg_color: "0a0f0b",
+    },
+    tokyonight: {
+        title_color: "70a5fd",
+        icon_color: "bf91f3",
+        text_color: "38bdae",
+        bg_color: "1a1b27",
+    },
+    onedark: {
+        title_color: "e4bf7a",
+        icon_color: "8eb573",
+        text_color: "df6d74",
+        bg_color: "282c34",
+    },
+    highcontrast: {
+        title_color: "e7f216",
+        icon_color: "00ffff",
+        text_color: "fff",
+        bg_color: "000",
+    },
+    dracula: {
+        title_color: "ff6e96",
+        icon_color: "79dafa",
+        text_color: "f8f8f2",
+        bg_color: "282a36",
+    },
+    monokai: {
+        title_color: "eb1f6a",
+        icon_color: "e28905",
+        text_color: "f1f1eb",
+        bg_color: "272822",
+    },
+    vue: {
+        title_color: "41b883",
+        icon_color: "41b883",
+        text_color: "273849",
+        bg_color: "fffefe",
+    },
+    vue_dark: {
+        title_color: "41b883",
+        icon_color: "41b883",
+        text_color: "fffefe",
+        bg_color: "273849",
+    },
+    nightowl: {
+        title_color: "c792ea",
+        icon_color: "ffeb95",
+        text_color: "7fdbca",
+        bg_color: "011627",
+    },
+    buefy: {
+        title_color: "7957d5",
+        icon_color: "ff3860",
+        text_color: "363636",
+        bg_color: "ffffff",
+    },
+    algolia: {
+        title_color: "00AEFF",
+        icon_color: "2DDE98",
+        text_color: "FFFFFF",
+        bg_color: "050F2C",
+    },
+};
+
+module.exports = color_themes;
