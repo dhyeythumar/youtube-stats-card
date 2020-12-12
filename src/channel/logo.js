d5dddcc80d7eb61820b9126d3a89de139aa1400b
Dhyey Thumar <dhyeythumar@gmail.com>
2020-12-12 21:50:06 +0530

    update channel logo holder

--- src/channel/logo.js
+++ src/channel/logo.js
@@ -1,4 +1,4 @@
-// Channel logo
+// Channel logo elements
 
 const getClipPath = () => {
     return `<clipPath id="imgholder" transform="translate(0 0)">
@@ -7,8 +7,8 @@ const getClipPath = () => {
 }
 
 const getLOGO = (url) => {
-    return `<image transform="translate(30 30)" width="80" height="80" href="${url}" clip-path="url(#imgholder)"/>
-    <g data-testid="card-logo" transform="translate(30 50)">
+    return `<image transform="translate(0 0)" width="80" height="80" href="${url}" clip-path="url(#imgholder)"/>
+    <g data-testid="card-logo" transform="translate(0 20)">
         <circle class="circle-rim" cx="40" cy="40" r="40"/>
     </g>`;
 }
