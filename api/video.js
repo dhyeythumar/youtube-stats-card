7989ef1f744a2f9256c1950d814ae9723d42a069
Dhyey Thumar <dhyeythumar@gmail.com>
2020-12-12 21:44:12 +0530

    add video api

---
+++ api/video.js
@@ -0,0 +1,53 @@
+const fetchVideoStats = require("../src/fetchers/videoStats-fetcher.js");
+const renderVideoStatsCard = require("../src/cards/video-card");
+const { renderError, parseBoolean, clampValue, CONSTANTS } = require("../src/common/utils");
+
+// :: TODO list ::
+// 1. Is there any other efficient way to render card layouts dynamically??
+
+module.exports = async (req, res) => {
+    const {
+        videoid,
+        layout,
+        hide_icons,
+        hide_preview,
+        hide_border,
+        title_color,
+        icon_color,
+        text_color,
+        bg_color,
+        theme,
+        cache_seconds,
+    } = req.query;
+
+    res.setHeader("Content-Type", "image/svg+xml");
+
+    try {
+        const stats = await fetchVideoStats(videoid);
+
+        // Cache not working in vercel dev env
+        const cacheSeconds = clampValue(
+            parseInt(cache_seconds || CONSTANTS.TWO_HOURS, 10),
+            CONSTANTS.TWO_HOURS,
+            CONSTANTS.ONE_DAY
+        );
+
+        res.setHeader("Cache-Control", `public, max-age=${cacheSeconds}`);
+
+        return res.send(
+            renderVideoStatsCard(stats, {
+                layout,
+                hide_icons: parseBoolean(hide_icons),
+                hide_preview: parseBoolean(hide_preview),
+                hide_border: parseBoolean(hide_border),
+                title_color,
+                icon_color,
+                text_color,
+                bg_color,
+                theme
+            })
+        );
+    } catch (err) {
+        return res.send(renderError(err.message, err.secondaryMessage));
+    }
+};
