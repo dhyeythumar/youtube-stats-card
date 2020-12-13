a01871a21208971b53b713a911e9dcc9f9b7cea7
Dhyey Thumar <dhyeythumar@gmail.com>
2020-12-13 15:10:34 +0530

    separate css style

---
+++ src/common/getStyles.js
@@ -0,0 +1,41 @@
+const getCommonStyle = (colors, addExtraCSS) => {
+    return `
+    .title {
+        font: 500 22px Ubuntu, 'Segoe UI', Sans-Serif;
+        fill: ${colors.titleColor};
+        animation: fadeInAnimation 0.8s ease-in-out forwards;
+    }
+    .card-bg {
+        fill: ${colors.bgColor};
+    }
+    .stagger {
+        opacity: 0;
+        animation: fadeInAnimation 0.3s ease-in-out forwards;
+    }
+    .text {
+        font: 500 16px Ubuntu, 'Segoe UI', Sans-Serif;
+        fill: ${colors.textColor};
+    }
+    .bold {
+        font-weight: 600;
+    }
+    .icon-color {
+        fill: ${colors.iconColor};
+        stroke: ${colors.iconColor};
+    }
+    .iconStroke-reduce {
+        stroke-width: 0.1px;
+        stroke: ${colors.bgColor};
+    }
+    @keyframes fadeInAnimation {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
+    }
+    ${addExtraCSS}`;
+}
+
+module.exports = { getCommonStyle };
